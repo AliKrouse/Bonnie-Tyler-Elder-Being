d143e6299142b011cb43319d94ec2f034e3d821f
Ali <alice.krouse@gmail.com>
2018-11-11 18:30:13 -0600

    last commit

---
+++ Assets/Scripts/brightEyes.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brightEyes : MonoBehaviour
+{
+    private GameObject[] eyeRotator = new GameObject[100];
+    public Material eyes;
+
+    private bool fadeIn, fadeOut;
+    public float fadeInSpeed, fadeOutSpeed;
+    private float a;
+
+    private void Awake()
+    {
+        for (int i = 0; i < 100; i++)
+            eyeRotator[i] = transform.GetChild(i).gameObject;
+    }
+
+    void OnEnable ()
+    {
+        foreach (GameObject g in eyeRotator)
+        {
+            float newY = Random.Range(0, 360);
+            float newX = Random.Range(-45, 45);
+            g.transform.eulerAngles = new Vector3(newX, newY, 0.0f);
+        }
+
+        fadeIn = true;
+    }
+	
+	void Update ()
+    {
+        if (fadeIn)
+        {
+            a = eyes.color.a;
+            a += Time.deltaTime * fadeInSpeed;
+            eyes.color = new Color(1, 1, 1, a);
+
+            if (a >= 1)
+            {
+                fadeOut = true;
+                fadeIn = false;
+            }
+        }
+        if (fadeOut)
+        {
+            a = eyes.color.a;
+            a -= Time.deltaTime * fadeOutSpeed;
+            eyes.color = new Color(1, 1, 1, a);
+
+            if (a <= 0)
+                this.gameObject.SetActive(false);
+        }
+	}
+}
---
+++ Assets/Scripts/camScriptVR.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class camScriptVR : MonoBehaviour
+{
+    private AudioSource source;
+    public AudioClip preGame;
+    public AudioClip[] clips;
+    public int i = 0;
+    
+    private bool gameBegun;
+
+    public GameObject rotationClone;
+    private GameObject frontFloater, backFloater;
+    private Vector3 target, lookPoint;
+    public float distance;
+
+    public GameObject brightEyes;
+
+    public Text t;
+    public GameObject forever;
+    
+    public Material moonMaterial;
+    private float time = 664;
+
+    public GameObject squid;
+    private Vector3 squidStart, squidEnd;
+
+    void Start ()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = preGame;
+        source.Play();
+
+        frontFloater = rotationClone.transform.GetChild(0).gameObject;
+        backFloater = rotationClone.transform.GetChild(1).gameObject;
+        target = backFloater.transform.position;
+
+        moonMaterial.color = new Color(1, 1, 1, 1);
+
+        squidStart = squid.transform.position;
+        squidEnd = new Vector3(0, 10000, 0);
+    }
+	
+	void Update ()
+    {
+        rotationClone.transform.eulerAngles = new Vector3(0.0f, transform.eulerAngles.y, 0.0f);
+        lookPoint = frontFloater.transform.position;
+        
+        distance = Vector3.Distance(lookPoint, target);
+        Debug.DrawLine(lookPoint, target, Color.red);
+        
+        if (!gameBegun)
+        {
+            if (distance < 0.25)
+            {
+                source.Stop();
+                gameBegun = true;
+                source.clip = clips[i];
+                source.Play();
+                t.gameObject.SetActive(false);
+            }
+        }
+        if (gameBegun)
+        {
+            if (source.isPlaying)
+            {
+                target = backFloater.transform.position;
+                time -= Time.deltaTime;
+            }
+            if (!source.isPlaying)
+            {
+                if (distance < 0.25 && i < 17)
+                {
+                    i++;
+                    source.clip = clips[i];
+                    source.Play();
+
+                    if (i == 5 || i == 6 || i == 11 || i == 12 || i == 13 || i == 14 || i == 15 || i == 16)
+                        brightEyes.SetActive(true);
+                }
+            }
+
+            moonMaterial.color = new Color(1, 1, 1, time / 664);
+
+            squid.transform.position = Vector3.Lerp(squidEnd, squidStart, time / 664);
+            
+            if (time <= 145)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Text text = forever.transform.GetChild(i).GetComponent<Text>();
+                    float a = text.color.a;
+                    a += Time.deltaTime;
+                    text.color = new Color(0.5f, 0, 0, a);
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            Time.timeScale = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            Time.timeScale = 2;
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            Time.timeScale = 3;
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            Time.timeScale = 4;
+
+        source.pitch = Time.timeScale;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+}
